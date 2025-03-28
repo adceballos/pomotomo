62781098446e507022e3287d77e4203008ae5b41
adceballos <78570037+adceballos@users.noreply.github.com>
2025-03-28 00:40:15 -0500

    Adjust about page styling

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,28 +3,29 @@ import BackToHome from "../components/BackToHome.jsx";
 function About() {
     return (
         <>
-            <div className="relative flex flex-col justify-between min-h-screen bg-[#ee906b] text-white px-4 py-20" id="about">
+            <div className="flex flex-col min-h-screen mt-12 text-black">
                 <BackToHome />
-                <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-[VT323] text-center">
+                <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center">
                     About the Pomodoro Technique
                 </h1>
                 
-                <div className="max-w-2xl mx-auto text-xl leading-relaxed space-y-6 py-12">
-                    <p>
-                        The <span className="text-[#abe8bd]">Pomodoro Technique</span> is a time management method that helps you focus and boost productivity 
+                <div className="max-w-2xl mx-auto text-2xl mt-12">
+                <h2 className="text-4xl font-semi-bold text-[#D5F0C0] mb-2">What is it?</h2>
+                    <p className="font-sans">
+                        The Pomodoro Technique is a time management method that helps you focus and boost productivity 
                         by breaking work into intervals, traditionally 25 minutes in length, separated by short breaks.
                     </p>
 
-                    <h2 className="text-4xl font-semi-bold text-[#abe8bd]">How It Works</h2>
-                    <ol className="list-decimal list-inside space-y-2">
+                    <h2 className="text-4xl font-semi-bold text-[#D5F0C0] mt-12 mb-2">How it works</h2>
+                    <ol className="font-sans list-decimal list-inside space-y-2">
                         <li>Choose a task you want to work on</li>
                         <li>Set a timer for 25 minutes and focus on the task</li>
                         <li>When the timer rings, take a 5-minute break</li>
                         <li>Repeat the cycle four times, then take a longer break (15-30 minutes)</li>
                     </ol>
 
-                    <h2 className="text-4xl font-semi-bold text-[#abe8bd]">Why It’s Effective</h2>
-                    <ul className="list-disc list-inside space-y-2">
+                    <h2 className="text-4xl font-semi-bold text-[#D5F0C0] mt-12 mb-2">Why it's effective</h2>
+                    <ul className="font-sans *:list-disc list-inside space-y-2">
                         <li>Reduces procrastination and distractions</li>
                         <li>Helps improve time estimation skills</li>
                         <li>Enhances focus and deep work</li>
